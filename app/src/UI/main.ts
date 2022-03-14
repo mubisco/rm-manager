23a3bf1325faee0ffd4d1f555c85a4ffa812280c
Xan Bellón <xan.bellon@gmail.com>
2022-03-15 00:34:06 +0100

    Added vuetify
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- app/src/UI/main.ts
+++ app/src/UI/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
+import vuetify from '@/UI/plugins/vuetify'
+import { loadFonts } from '@/UI/plugins/webfontloader'
 import router from '@/UI/router/index'
 import App from '@/UI/App.vue'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
