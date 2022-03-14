a53853cf70f87e30c6a4321c953fd7fa18b22b41
Xan Bellón <xan.bellon@gmail.com>
2022-03-14 20:16:34 +0100

    Added esling
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- app/src/UI/App.vue
+++ app/src/UI/App.vue
@@ -5,7 +5,10 @@ import HelloWorld from '@/UI/components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="@/UI/assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="@/UI/assets/logo.png"
+  >
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
