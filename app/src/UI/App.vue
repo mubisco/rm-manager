33249cde026b43d68504600bbfb0adbeb5c078f3
Xan Bellón <xan.bellon@gmail.com>
2022-03-15 01:03:25 +0100

    Defined base app bar
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- app/src/UI/App.vue
+++ app/src/UI/App.vue
@@ -1,20 +1,50 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
 <template>
-  <div class="flex">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <div class="text-center">
-      <img
-        class="mx-auto mt-2"
-        alt="Vue logo"
-        src="@/UI/assets/logo.png"
-      >
-    </div>
-  </div>
-  <router-view />
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+    >
+      <template #prepend>
+        <v-app-bar-nav-icon color="primary" />
+      </template>
+
+      <v-app-bar-title>Photos</v-app-bar-title>
+
+      <template #append>
+        <v-btn
+          icon="mdi-account"
+          color="primary"
+          to="/login"
+        />
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
