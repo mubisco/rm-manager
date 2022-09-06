1717aca8192721af081fc7238b440e21b91735e1
Xan Bellón <xan.bellon@gmail.com>
2022-09-07 00:52:58 +0200

    Implemented basic tcr.sh
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

---
+++ tcr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Running tests..."
+git add back/tests
+git commit -m 'Commiting tests'
+make tests-back &> /dev/null
+if [ $? -eq 0 ]; then
+  git add back
+  git commit -m 'Tests passed - Commited'
+  echo "Tests passed - Changes commited"
+else
+  git checkout back
+  echo "Tests not passed. Reverting!!!"
+fi
