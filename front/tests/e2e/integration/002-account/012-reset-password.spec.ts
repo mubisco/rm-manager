85e7b36f3c5707d8c45c287c8b2862670e473a26
Xan Bellón <xan.bellon@gmail.com>
2022-10-22 02:07:39 +0200

    Refactored tests

--- front/tests/e2e/integration/002-account/012-reset-password.spec.ts
+++ front/tests/e2e/integration/002-account/012-reset-password.spec.ts
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
 import routes from '../../fixtures/routes.json'
 
+const mockSuccessfulTokenCheck = () => {
+  cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
+    delay: 500,
+    body: { message: 'NOT_AUTH' },
+    statusCode: 200
+  }).as('checkTokenUrl')
+}
+const fillPasswordProperly = () => {
+  cy.get('[data-cy="new-password"]').type('onePassword')
+  cy.get('[data-cy="confirm-new-password"]').type('onePassword');
+}
+
 describe('GIVEN a non registered user', () => {
   context('WHEN I navigate to reset password page', () => {
     beforeEach(() => {
@@ -27,11 +39,7 @@ describe('GIVEN a non registered user', () => {
   })
   context('WHEN I navigate to with a valid token', () => {
     it('THEN I should see reset password form', () => {
-      cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
-        delay: 500,
-        body: { message: 'NOT_AUTH' },
-        statusCode: 200
-      }).as('checkTokenUrl')
+      mockSuccessfulTokenCheck()
       cy.visit(routes.front + '/reset-password/a-very-large-token')
       cy.wait('@checkTokenUrl').then(() => {
         cy.get('[data-cy="change-password-form"]').should('exist');
@@ -41,80 +49,61 @@ describe('GIVEN a non registered user', () => {
         cy.get('[data-cy="send-new-password"]').should('be.disabled')
       })
     })
-    context('AND I fill the two fields with different passwords', () => {
-      it('THEN I should see and warning error and button should be disabled', () => {
-        cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 200
-        }).as('checkTokenUrl')
-        cy.visit(routes.front + '/reset-password/a-very-large-token')
-        cy.wait('@checkTokenUrl').then(() => {
-          cy.get('[data-cy="new-password"]').type('onePassword')
-          cy.get('[data-cy="confirm-new-password"]').type('anotherPassword');
-          cy.get('[data-cy="confirm-new-password"]').should('have.class', 'v-input--error')
-          cy.get('[data-cy="send-new-password"]').should('be.disabled')
+  })
+  context('WHEN I fill the two fields with different passwords', () => {
+    it('THEN I should see and warning error and button should be disabled', () => {
+    mockSuccessfulTokenCheck()
+      cy.visit(routes.front + '/reset-password/a-very-large-token')
+      cy.wait('@checkTokenUrl').then(() => {
+        cy.get('[data-cy="new-password"]').type('onePassword')
+        cy.get('[data-cy="confirm-new-password"]').type('anotherPassword');
+        cy.get('[data-cy="confirm-new-password"]').should('have.class', 'v-input--error')
+        cy.get('[data-cy="send-new-password"]').should('be.disabled')
+      })
+    })
+  })
+  context('WHEN I fill the two fields with same passwords', () => {
+    it('THEN send button should be enabled', () => {
+      mockSuccessfulTokenCheck()
+      cy.visit(routes.front + '/reset-password/a-very-large-token')
+      cy.wait('@checkTokenUrl').then(() => {
+        fillPasswordProperly()
+        cy.get('[data-cy="send-new-password"]').should('be.enabled')
+      })
+    })
+  })
+  context('AND WHEN there is an error sending password', () => {
+    it('THEN send button should be enabled', () => {
+      mockSuccessfulTokenCheck()
+      cy.intercept('POST', routes.back + '/api/account/reset-password', {
+        delay: 500,
+        body: { message: 'NOT_AUTH' },
+        statusCode: 500
+      }).as('changePasswordUrl')
+      cy.visit(routes.front + '/reset-password/a-very-large-token')
+      cy.wait('@checkTokenUrl').then(() => {
+        fillPasswordProperly()
+        cy.get('[data-cy="send-new-password"]').click()
+        cy.wait('@changePasswordUrl').then(() => {
+          cy.get('[data-cy="token-error"]').should('exist');
         })
       })
     })
-    context('AND I fill the two fields with same passwords', () => {
-      it('THEN send button should be enabled', () => {
-        cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 200
-        }).as('checkTokenUrl')
-        cy.visit(routes.front + '/reset-password/a-very-large-token')
-        cy.wait('@checkTokenUrl').then(() => {
-          cy.get('[data-cy="new-password"]').type('onePassword')
-          cy.get('[data-cy="confirm-new-password"]').type('onePassword')
-          cy.get('[data-cy="send-new-password"]').should('be.enabled')
-        })
-      })
-    })
-    context('AND there is an error sending password', () => {
-      it('THEN send button should be enabled', () => {
-        cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 200
-        }).as('checkTokenUrl')
-        cy.intercept('POST', routes.back + '/api/account/reset-password', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 500
-        }).as('changePasswordUrl')
-        cy.visit(routes.front + '/reset-password/a-very-large-token')
-        cy.wait('@checkTokenUrl').then(() => {
-          cy.get('[data-cy="new-password"]').type('onePassword')
-          cy.get('[data-cy="confirm-new-password"]').type('onePassword');
-          cy.get('[data-cy="send-new-password"]').click()
-          cy.wait('@changePasswordUrl').then(() => {
-            cy.get('[data-cy="token-error"]').should('exist');
-          })
-        })
-      })
-    })
-    context('AND password is reset', () => {
-      it('THEN send button should be enabled', () => {
-        cy.intercept('GET', routes.back + '/api/account/check-token/a-very-large-token', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 200
-        }).as('checkTokenUrl')
-        cy.intercept('POST', routes.back + '/api/account/reset-password', {
-          delay: 500,
-          body: { message: 'NOT_AUTH' },
-          statusCode: 200
-        }).as('changePasswordUrl')
-        cy.visit(routes.front + '/reset-password/a-very-large-token')
-        cy.wait('@checkTokenUrl').then(() => {
-          cy.get('[data-cy="new-password"]').type('onePassword')
-          cy.get('[data-cy="confirm-new-password"]').type('onePassword');
-          cy.get('[data-cy="send-new-password"]').click()
-          // cy.get('[data-cy="token-success"]').should('exist');
-          cy.location('pathname').should('match', /\/login/);
-        })
+  })
+  context('AND WHEN password is reset', () => {
+    it('THEN send button should be enabled', () => {
+      mockSuccessfulTokenCheck()
+      cy.intercept('POST', routes.back + '/api/account/reset-password', {
+        delay: 500,
+        body: { message: 'NOT_AUTH' },
+        statusCode: 200
+      }).as('changePasswordUrl')
+      cy.visit(routes.front + '/reset-password/a-very-large-token')
+      cy.wait('@checkTokenUrl').then(() => {
+        fillPasswordProperly()
+        cy.get('[data-cy="send-new-password"]').click()
+        // cy.get('[data-cy="token-success"]').should('exist');
+        cy.location('pathname').should('match', /\/login/);
       })
     })
   })
