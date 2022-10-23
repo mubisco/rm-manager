03a4370f52e2e9eaca5e9bf699e2578b51ab6efc
Xan Bellón <xan.bellon@gmail.com>
2022-10-23 18:03:00 +0200

    Added SendPasswordTokenEventHandler to EventQueue
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- back/src/User/Application/Event/SendPasswordTokenEventHandler.php
+++ back/src/User/Application/Event/SendPasswordTokenEventHandler.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\User\Application\Event;
 
+use App\Shared\Application\EventHandlerInterface;
 use App\User\Domain\PasswordTokenMailer;
 use App\User\Domain\PasswordTokenWasRequested;
 use App\User\Domain\UserId;
 use App\User\Domain\UserRepository;
 
-final class SendPasswordTokenEventHandler
+final class SendPasswordTokenEventHandler implements EventHandlerInterface
 {
     public function __construct(
         private UserRepository $userRepository,
