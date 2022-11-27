f2e597d14543c4153c08cd81b1e125096ba963f3
Xan Bellón <xan.bellon@gmail.com>
2022-11-27 12:32:00 +0100

    Completed ReadyControllerTest
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- back/tests/unit/Ready/Infrastructure/EntryPoint/Api/ReadyControllerTest.php
+++ back/tests/unit/Ready/Infrastructure/EntryPoint/Api/ReadyControllerTest.php
@@ -7,12 +7,50 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ReadyControllerTest extends TestCase
 {
+    private ReadyController $sut;
+
+    protected function setUp(): void
+    {
+        $this->sut = new ReadyController();
+    }
+
     public function testShouldReturnJsonResponse(): void
     {
-        $sut = new ReadyController();
-        $result = $sut->index();
+        $result = $this->sut->index();
+        $this->checkProperRespose($result, '{"ready":true}');
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnJsonResponseWhenCallingAdmin(): void
+    {
+        $result = $this->sut->admin();
+        $this->checkProperRespose($result, '{"admin":true}');
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnJsonResponseWhenCallingMaster(): void
+    {
+        $result = $this->sut->master();
+        $this->checkProperRespose($result, '{"master":true}');
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnProperJsonResponseWhenCallingPlayer(): void
+    {
+        $result = $this->sut->player();
+        $this->checkProperRespose($result, '{"player":true}');
+    }
+
+    private function checkProperRespose(mixed $result, string $expectedResponse): void
+    {
         $this->assertInstanceOf(JsonResponse::class, $result);
         $this->assertEquals(200, $result->getStatusCode());
-        $this->assertEquals('{"ready":true}', $result->getContent());
+        $this->assertEquals($expectedResponse, $result->getContent());
     }
 }
