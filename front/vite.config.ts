e81ce2ecebd3f3199f59eed33378c99f071dddc5
Xan Bellón <xan.bellon@gmail.com>
2023-01-08 12:51:43 +0100

    Updated npm packages
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,16 +3,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import path from 'path'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vuetify({
-      autoImport: true,
-    }),
-    vueI18n({
+    vuetify({ autoImport: true }),
+    VueI18nPlugin({
       globalSFCScope: true,
       include: path.resolve(__dirname, './src/UI/plugins/locales/**')
     })
@@ -24,8 +22,8 @@ export default defineConfig({
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': path.resolve(__dirname, 'src')
+    }
   },
   test: {
     environment: 'jsdom',
@@ -33,7 +31,7 @@ export default defineConfig({
     setupFiles: 'tests/test-setup.js',
     clearMocks: true,
     deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi', 'vuetify'],
+      inline: ['@vue', '@vueuse', 'vue-demi', 'vuetify']
     }
   }
 })
