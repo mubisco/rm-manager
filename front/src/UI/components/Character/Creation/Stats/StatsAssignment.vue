c55a3478ae5dd409f6e55223fc085c3f8dde2c10
Xan Bellón <xan.bellon@gmail.com>
2023-03-19 15:38:20 +0100

    Added column for stats roll

--- front/src/UI/components/Character/Creation/Stats/StatsAssignment.vue
+++ front/src/UI/components/Character/Creation/Stats/StatsAssignment.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { watch, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useDicebagStore } from '@/UI/stores/dicebag'
+const dicebagStore = useDicebagStore()
+const { totalRolled } = storeToRefs(dicebagStore)
+
+watch(totalRolled, () => {
+  rolledResults.value = dicebagStore.breakdown
+})
 
 defineProps<{ availablePoints: number, selectedMethod: string }>()
 const statsCodes = ref(['ST', 'CO', 'AG', 'QU', 'SD', 'RE', 'IN', 'PR'])
+const rolledResults = ref<number[]>([])
 </script>
 <template>
   <div>
     <p>{{ selectedMethod }} - {{ availablePoints }}</p>
     <v-row>
       <v-col cols="2">
+        <div>
+          <v-chip
+            v-for="(roll, index) in rolledResults"
+            :key="index"
+            draggable
+            variant="tonal"
+            color="primary"
+          >
+            {{ roll }}
+          </v-chip>
+        </div>
+        <v-btn
+          block
+          prepend-icon="mdi-dice-d10-outline"
+          class="mr-4"
+          @click="dicebagStore.requestRoll('8d100')"
+        >
+          Roll 8D100
+        </v-btn>
       </v-col>
       <v-col cols="10">
         <v-table>
