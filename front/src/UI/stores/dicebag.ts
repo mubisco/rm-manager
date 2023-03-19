510f4a01bdccf5eefeef8c1a56d768f1de7a9f9f
Xan Bellón <xan.bellon@gmail.com>
2023-03-19 15:37:58 +0100

    Adapted RollResultDto to store

--- front/src/UI/stores/dicebag.ts
+++ front/src/UI/stores/dicebag.ts
@@ -5,20 +5,22 @@ export const useDicebagStore = defineStore('dicebag', {
   state: () => ({
     totalRolled: 0,
     dicebagDefinition: '',
-    breakdown: [] as number[]
+    rollBreakdown: [] as number[]
   }),
   getters: {
     rolledValue: state => state.totalRolled,
-    definition: state => state.dicebagDefinition
+    definition: state => state.dicebagDefinition,
+    breakdown: state => state.rollBreakdown
   },
   actions: {
     requestRoll (definition: string): void {
       this.dicebagDefinition = definition
     },
     updateResult (rolledResult: RollResultDto): void {
+      const parsedResult = JSON.parse(JSON.stringify(rolledResult))
       this.dicebagDefinition = ''
       this.totalRolled = rolledResult.total
-      this.breakdown = rolledResult.rollBreakdown
+      this.rollBreakdown = parsedResult.rollBreakdown
     }
   }
 })
