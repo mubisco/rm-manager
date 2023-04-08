12fd2bdd84e6f5c2516f1c3acb719719bcb89c7f
Xan Bellón <xan.bellon@gmail.com>
2023-04-08 21:56:42 +0200

    Second version of racial stats table
    Signed-off-by: Xan Bellón <xan.bellon@gmail.com>

--- front/src/UI/components/Character/Creation/Race/RacialStatsTable.vue
+++ front/src/UI/components/Character/Creation/Race/RacialStatsTable.vue
@@ -11,57 +11,112 @@ defineProps<{
 
 </script>
 <template>
-  <v-row>
-    <v-col cols="6">
-      <v-table>
-        <thead>
-          <tr>
-            <th>Característica</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(stat, key) in statModifiers"
-            :key="key"
-          >
-            <td>{{ $t('character.stats.' + key) }}</td>
-            <td>{{ stat }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-col>
-    <v-col cols="6">
-      <v-table>
-        <thead>
-          <tr>
-            <th>Estadística</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Aguante</td>
-            <td>{{ endurance }}</td>
-          </tr>
-          <tr>
-            <td>Puntos de Poder</td>
-            <td>{{ powerPoints }}</td>
-          </tr>
-          <tr>
-            <td>Resistencia Física</td>
-            <td>{{ resistanceBonuses.stamina }}</td>
-          </tr>
-          <tr>
-            <td>Resistencia Voluntad</td>
-            <td>{{ resistanceBonuses.will }}</td>
-          </tr>
-          <tr>
-            <td>Resistencia Magia</td>
-            <td>{{ resistanceBonuses.magic }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-col>
-  </v-row>
+  <v-table>
+    <thead>
+      <tr>
+        <th
+          colspan="8"
+          class="text-center"
+        >
+          Características
+        </th>
+      </tr>
+      <tr>
+        <th>{{ $t('character.stats.ST') }}</th>
+        <th>{{ $t('character.stats.CO') }}</th>
+        <th>{{ $t('character.stats.AG') }}</th>
+        <th>{{ $t('character.stats.QU') }}</th>
+        <th>{{ $t('character.stats.SD') }}</th>
+        <th>{{ $t('character.stats.RE') }}</th>
+        <th>{{ $t('character.stats.IN') }}</th>
+        <th>{{ $t('character.stats.PR') }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-center">
+          {{ statModifiers.ST }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.CO }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.AG }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.QU }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.SD }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.RE }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.IN }}
+        </td>
+        <td class="text-center">
+          {{ statModifiers.PR }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+  <v-alert
+    v-if="statModifiers.ST === '*'"
+    variant="tonal"
+    type="info"
+    density="compact"
+  >
+    Humans do not have fixed racial bonuses. Players of a human character have 10 points to divide any way they like between the 8 racial stat bonuses. No stat may have a bonus greater than +3
+  </v-alert>
+  <v-table>
+    <thead>
+      <tr>
+        <th />
+        <th />
+        <th
+          colspan="3"
+          class="text-center"
+        >
+          Bono Resistencia
+        </th>
+      </tr>
+      <tr>
+        <th class="text-center">
+          Aguante
+        </th>
+        <th class="text-center">
+          Puntos de Poder
+        </th>
+        <th class="text-center">
+          Resistencia
+        </th>
+        <th class="text-center">
+          Voluntad
+        </th>
+        <th class="text-center">
+          Magia
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="text-center">
+          {{ endurance }}
+        </td>
+        <td class="text-center">
+          {{ powerPoints }}
+        </td>
+        <td class="text-center">
+          {{ resistanceBonuses.stamina }}
+        </td>
+        <td class="text-center">
+          {{ resistanceBonuses.will }}
+        </td>
+        <td class="text-center">
+          {{ resistanceBonuses.magic }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
